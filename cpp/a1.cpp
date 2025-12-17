f0d337755e05d7759347303322a00f788fb0e17b
Kazus <120459752+GTAlex1@users.noreply.github.com>
2025-12-17 12:38:39 +0100

    a

---
+++ cpp/a1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int insectos;
+    int aracnidos;
+    int crustaceos;
+    int miriapodos;
+    int anillos;
+    
+    cout << "¿Cuántos insectos hay? ";
+    cin >> insectos;
+    cout << "¿Cuántos arácnidos hay? ";
+    cin >> aracnidos;
+    cout << "¿Cuántos crustáceos hay? ";
+    cin >> crustaceos;
+    cout << "¿Cuántas escalopendras hay? ";
+    cin >> miriapodos;
+    cout << "¿Cuántos anillos tienen? ";
+    cin >> anillos;
+    
+    cout << "Hay " << 6 * insectos + 8 * aracnidos + 10 * crustaceos + 2 * anillos * miriapodos << " patas en total.";
+}
