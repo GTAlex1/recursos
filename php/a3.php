3f85486811bd7f0477d006cc56c3077e83edb3ae
Kazus <120459752+GTAlex1@users.noreply.github.com>
2026-01-15 12:47:11 +0100

    a

---
+++ php/a3.php
@@ -0,0 +1,19 @@
+<!-- Crea un formulario que pida a un usuario elnúmero de calificaciones a ingresar, después mostrar otro formulario
+con las calificaciones solicitadas para que ingrese los valores, y finalmente mostrar el promedio de dicha las calificaciones (si es mayor
+o igual a 5) o poner “Suspenso” (si menor a 5). Usar un único archivo PHP -->
+
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
